d8347d8f6c7243b41ce763d68bafc0508d69cfca
Harsh Patel <your_github_harshthummar542@gmail.com>
2025-09-16 18:06:42 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,18 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+module.exports = app;
