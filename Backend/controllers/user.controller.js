3f1d49e905183cc809393530266f36dc753faf82
Harsh Patel <your_github_harshthummar542@gmail.com>
2025-09-17 15:08:11 +0530

    user login

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -2,6 +2,8 @@ const { validationResult } = require("express-validator");
 const userService = require('../services/user.service');
 const userModel = require("../models/user.model");
 
+
+// registetion controller
 module.exports.registerUser = async (req, res, next) => {
   try {
     const errors = validationResult(req);
@@ -31,3 +33,26 @@ module.exports.registerUser = async (req, res, next) => {
     res.status(500).json({ message: "Server error", error: error.message });
   }
 };
+
+// login controller
+module.exports.loginUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+  const user = await userModel.findOne({email}).select('+password');
+
+  if (!user) {
+    return res.status(401).json({ message: "Invalid email or password" });
+  }
+
+  const isMatch = await user.comparePassword(password);
+  if (!isMatch) {
+    return res.status(401).json({ message: "Invalid email or password" });
+  }
+
+  const token = await user.generateAuthToken();
+  res.status(200).json({ token, user });
+}
